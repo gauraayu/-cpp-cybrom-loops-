a12a451f228d915f2524d95f11b1efb2fe5bba99
gauraayu <gauraayushi2004@gmail.com>
2024-12-01 20:07:51 +0530

    update

--- 15 nov/reverse.cpp
+++ 15 nov/reverse.cpp
@@ -3,14 +3,17 @@ using namespace std;
 int main()
 {
   int n;
-  int rev;
-  int r;
+  int rev=0;
+  int r=0;
   cout<<"enter n";
   cin>>n;
-  for(int i=1;i>0;++i){
-    r=n
+  for(int i=0;n>0;++i){
+    r=n%10;
+    rev=rev*10+r;
+    n=n/10;
  
 
   }
+  cout<<rev;
 
 }
